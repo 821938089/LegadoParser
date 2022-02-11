eb8a9f43a10c3c6eb12cbee0edd2c2a18999f9ac
821938089 <821938089@qq.com>
2022-02-11 15:03:59 +0800

    Update JS.py

--- LegadoParser2/RuleJs/JS.py
+++ LegadoParser2/RuleJs/JS.py
@@ -19,7 +19,8 @@ class EvalJs(object):
         self.context = quickjs.Context()
         self.VAR = {}  # 存放put get方法的内容
         if not _jsCache:
-            with open(os.path.join(__file__, '../jsExtension.js'), 'r') as f:
+            filePath = os.path.dirname(os.path.abspath('__file__'))
+            with open(os.path.join(filePath, '../jsExtension.js'), 'r') as f:
                 _jsCache = f.read()
                 self.context.eval(_jsCache)
         else:
