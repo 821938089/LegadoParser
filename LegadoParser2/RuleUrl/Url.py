f5b88abadbdbc751c308d66098576074a2754df9
821938089 <821938089@qq.com>
2022-02-12 21:35:28 +0800

    update

--- LegadoParser2/RuleUrl/Url.py
+++ LegadoParser2/RuleUrl/Url.py
@@ -7,7 +7,7 @@ from LegadoParser2.RuleType import RuleType
 from LegadoParser2.RuleUrl.BodyType import Body
 from LegadoParser2.RuleUrl.UrlEval import getUrlRuleObj, getString
 from LegadoParser2.webview import WebView
-from LegadoParser2.config import DEBUG_MODE
+from LegadoParser2.config import DEBUG_MODE, USER_AGENT
 
 
 def parseUrl(ruleUrl, evalJs, baseUrl='', headers=''):
@@ -78,8 +78,7 @@ def parseUrl(ruleUrl, evalJs, baseUrl='', headers=''):
 def setDefaultHeaders(headers, bodyType):
     headerKeys = [k.lower() for k in headers.keys()]
     if 'user-agent' not in headerKeys:
-        headers[
-            'User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        headers['User-Agent'] = USER_AGENT
     if 'content-type' not in headerKeys:
         if bodyType == Body.FORM:
             headers['Content-Type'] = 'application/x-www-form-urlencoded'
@@ -117,12 +116,13 @@ def getContent(urlObj):
     url = urlparse(urlObj['url'])
     url = url._replace(query=urlencode(parse_qs(url.query), doseq=True, encoding=charset))
     url = urlunparse(url)
+    userAgent = urlObj['headers']['User-Agent']
     if body and bodyType == Body.FORM:
         body = urlencode(parse_qs(body), doseq=True, encoding=charset)
     elif body:
         body = body.encode(charset)
     if urlObj['webView'] and urlObj['method'] == 'GET':
-        webView = WebView()
+        webView = WebView(userAgent)
         content = webView.getResponseByUrl(url, urlObj['webJs'])
         respone = None
 
@@ -131,7 +131,7 @@ def getContent(urlObj):
                                    method=method, post_data=body)
 
     if urlObj['webView'] and urlObj['method'] == 'POST':
-        webView = WebView()
+        webView = WebView(userAgent)
         content = webView.getResponseByHtml(content, urlObj['webJs'])
         respone = None
 
--- LegadoParser2/config.py
+++ LegadoParser2/config.py
@@ -1,6 +1,9 @@
-# 配置运行环境模式
+# 常量存放
 
-DEBUG_MODE = False
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+
+DEBUG_MODE = True
 
 if DEBUG_MODE:
     print('!!! 警告:你正处于调试模式下运行')
