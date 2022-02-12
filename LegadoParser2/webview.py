2182e7943c7daf0e8000bc5c3cef7cbdf049bdac
821938089 <821938089@qq.com>
2022-02-12 22:47:22 +0800

    update

--- LegadoParser2/webview.py
+++ LegadoParser2/webview.py
@@ -4,6 +4,7 @@ import sys
 import base64
 import logging
 import time
+import atexit
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -63,45 +64,28 @@ class WebView(object):
             return self.driver.page_source
 
 
+@atexit.register
+def closeBroswer():
+    global _driver
+    if _driver:
+        _driver.quit()
+
+
 def getDriverInstance(userAgent=USER_AGENT):
-    chromePath = getChromePath()
-    if sys.platform == 'win32' and chromePath:
-        user_data_dir = os.path.join(os.path.abspath("."), 'webview\AutomationProfile')
-
-        if DEBUG_MODE:
-            subprocess.Popen(
-                f'cmd /c ""{chromePath}" --remote-debugging-port=9222 --user-data-dir="{user_data_dir}" --lang=zh-CN --mute-audio --user-agent="{userAgent}""')
-        else:
-            subprocess.Popen(
-                f'cmd /c ""{chromePath}" --remote-debugging-port=9222 --user-data-dir="{user_data_dir}" --lang=zh-CN --headless --disable-gpu --mute-audio --user-agent="{userAgent}""')
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        options.set_capability("detach", True)
-        service = Service(executable_path=ChromeDriverManager(log_level=logging.NOTSET).install())
-        driver = webdriver.Chrome(service=service, options=options)
-    else:
-        options = webdriver.ChromeOptions()
-        options.set_capability("detach", True)
-        if not DEBUG_MODE:
-            options.headless = True
-        options.add_argument("--mute-audio")
+    options = webdriver.ChromeOptions()
+    user_data_dir = os.path.join(os.path.abspath("."), 'webview\AutomationProfile')
+    # options.set_capability("detach", True)
+    if not DEBUG_MODE:
+        options.headless = True
         options.add_argument("--headless")
-        # 去除webdriver痕迹
-        # https://zhuanlan.zhihu.com/p/328768200
-        options.add_argument("disable-blink-features=AutomationControlled")
-        options.add_argument(f"user-agent={userAgent}")
-        service = Service(executable_path=ChromeDriverManager(log_level=logging.NOTSET).install())
-        driver = webdriver.Chrome(service=service, options=options)
+        options.add_argument("--disable-gpu")
+    options.add_argument("--mute-audio")
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+    options.add_argument("--lang=zh-CN,zh")
+    # 去除webdriver痕迹
+    # https://zhuanlan.zhihu.com/p/328768200
+    options.add_argument("disable-blink-features=AutomationControlled")
+    options.add_argument(f"user-agent={userAgent}")
+    service = Service(executable_path=ChromeDriverManager(log_level=logging.NOTSET).install())
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
-
-
-def getChromePath():
-    if sys.platform == 'win32':
-        chromePath = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-        if not os.path.exists(chromePath):
-            chromePath = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-        if not os.path.exists(chromePath):
-            chromePath = ""
-    else:
-        chromePath = ""
-    return chromePath
