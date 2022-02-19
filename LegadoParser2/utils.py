b59cc4fc390db75edb1ea7604f890199782dfd12
821938089 <821938089@qq.com>
2022-02-19 14:45:24 +0800

    Update utils.py

--- LegadoParser2/utils.py
+++ LegadoParser2/utils.py
@@ -1,11 +1,11 @@
 def validateFlag(flag):
     """
     将 isPay isVip isVolume 转换为布尔值
-    当 flag 为"None" "False" "0" ""时返回False
+    当 flag 为"None" "False" "0" "" "null"时返回False
     其余情况返回True
     """
-    falseSet = {'None', 'False', '0', ''}
-    if flag in falseSet:
+    falseSet = {'none', 'false', '0', '', 'null'}
+    if flag.lower() in falseSet:
         return False
     else:
         return True
