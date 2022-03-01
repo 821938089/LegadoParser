fa28442db7d6901b3e6a39af64475895223ce040
821938089 <821938089@qq.com>
2022-03-01 11:21:40 +0800

    Update config.py

--- LegadoParser2/config.py
+++ LegadoParser2/config.py
@@ -1,13 +1,12 @@
 # 常量存放
 
-from webdriver_manager.chrome import ChromeDriverManager
-import logging
+from webdriver_manager.utils import get_browser_version_from_os, ChromeType
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
 DEBUG_MODE = True
 
-CAN_USE_WEBVIEW = ChromeDriverManager(log_level=logging.NOTSET).driver.browser_version != 'UNKNOWN'
+CAN_USE_WEBVIEW = get_browser_version_from_os(ChromeType.GOOGLE) != 'UNKNOWN'
 
 if DEBUG_MODE:
     print('!!! 警告:你正处于调试模式下运行')
